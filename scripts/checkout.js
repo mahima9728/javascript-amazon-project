97891c8b65790a62c26455cc5344717c235a71cc
mahima9728 <mahimarawat979@gmail.com>
2025-05-24 04:32:11 +0530

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import {
   removeFromCart,
   calculateCartQuantity,
   updateQuantity,
+  updateDeliveryOption,
 } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formateCurrency } from "./utils/money.js";
@@ -27,11 +28,11 @@ cart.forEach((cartItem) => {
     }
   });
 
-  const deliveryOptionsId = cartItem.deliveryOptionsId;
+  const deliveryOptionId = cartItem.deliveryOptionId;
 
   let deliveryOption;
   deliveryOptions.forEach((option) => {
-    if (option.id === deliveryOptionsId) {
+    if (option.id === deliveryOptionId) {
       deliveryOption = option;
     }
   });
@@ -102,9 +103,12 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         ? "FREE"
         : `$${formateCurrency(deliveryOption.priceCents)}-`;
 
-    const isChecked = deliveryOption.id === cartItem.deliveryOptionsId;
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
-    html += ` <div class="delivery-option">
+    html += ` <div class="delivery-option js-delivery-option" data-product-id ="${
+      matchingProduct.id
+    }"
+    data-delivery-option-id ="${deliveryOption.id}">
                   <input
                     type="radio"
                     ${isChecked ? "checked" : ""}
@@ -180,5 +184,12 @@ document.querySelectorAll(".js-save-link").forEach((link) => {
     quantityLabel.innerHTML = newQuantity;
 
     updateCartQuantity();
+  });
+});
+
+document.querySelectorAll(".js-delivery-option").forEach((element) => {
+  element.addEventListener("click", () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });

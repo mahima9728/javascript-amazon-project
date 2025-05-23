c093116e3545b5e6ff50235363266aac2dad134e
mahima9728 <mahimarawat979@gmail.com>
2025-05-24 01:54:15 +0530

    Did exercises

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formateCurrency } from "./utils/money.js";
 
@@ -63,11 +63,12 @@ products.forEach((product) => {
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 function updateCartQuantity() {
-  let cartQuantity = 0;
+  // let cartQuantity = 0;
+  let cartQuantity = calculateCartQuantity();
 
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+  // cart.forEach((cartItem) => {
+  //   cartQuantity += cartItem.quantity;
+  // });
 
   document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 }

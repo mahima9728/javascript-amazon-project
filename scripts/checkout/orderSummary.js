16486328a18882cc6edc2c0bd6f24137146f33bb
mahima9728 <mahimarawat979@gmail.com>
2025-05-24 05:58:57 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -13,10 +13,8 @@ import {
   deliveryOptions,
   getDeliveryOption,
 } from "../../data/deliveryOptions.js";
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
-console.log(deliveryDate.format("dddd, MMMM D "));
+
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -129,7 +127,8 @@ export function renderOrderSummary() {
         `.js-cart-item-container-${productId}`
       );
       container.remove();
-      updateCartQuantity();
+      renderPaymentSummary();
+      // updateCartQuantity();
     });
   });
   function updateCartQuantity() {
@@ -185,6 +184,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary(); //recursion
+      renderPaymentSummary();
     });
   });
 }

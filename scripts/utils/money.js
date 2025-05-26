27dd3383b23b1b57c6a96eb530bb8830d06a791f
mahima9728 <mahimarawat979@gmail.com>
2025-05-26 19:32:32 +0530

    Create automated tests for formateCurrency

--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,5 @@
 export function formateCurrency(priceCents) {
   return (Math.round(priceCents) / 100).toFixed(2);
 }
+
+export default formateCurrency;
---
+++ tests/moneyTest.js
@@ -0,0 +1,24 @@
+import { formateCurrency } from "../scripts/utils/money.js";
+
+///TestCase 1
+
+if (formateCurrency(2095) === "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+///TestCase 2
+
+if (formateCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+///TestCase 3
+if (formateCurrency(2000.5) === "20.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}

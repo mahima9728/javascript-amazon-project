b3b3d9e06b0f292bce9ab3e8c48e5432f7bbed46
mahima9728 <mahimarawat979@gmail.com>
2025-05-29 15:18:56 +0530

    Use inheritance and polymorphism to add size chart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -26,7 +26,7 @@ products.forEach((product) => {
             }</div>
           </div>
 
-          <div class="product-price">${product.getPRice()}</div>
+          <div class="product-price">${product.getPrice()}</div>
 
           <div class="product-quantity-container">
             <select>
@@ -42,6 +42,8 @@ products.forEach((product) => {
               <option value="10">10</option>
             </select>
           </div>
+
+          ${product.extraInfoHTML()}
 
           <div class="product-spacer"></div>
 
